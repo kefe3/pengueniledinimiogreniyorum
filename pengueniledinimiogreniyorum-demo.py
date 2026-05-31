76d78532c76f4ea6226eeb610ba8184d627809f4
Kağan Efe TOPÇU <267961796+kefe3@users.noreply.github.com>
2026-05-31 20:00:08 +0300

    Projemizin ilk çıkan dosyası Demo hali
    Projemizin sadece 2 özelliği çalıştırılabilen demo hali.

---
+++ pengueniledinimiogreniyorum-demo.py
@@ -0,0 +1,160 @@
+sureler = {
+    "Fatiha Suresi": {
+        "okunus": "Bismillâhirrahmanirrahim. Elhamdülillâhi rabbil’alemin. Errahmânir’rahim. Mâliki yevmiddin. İyyâke na’budu ve iyyâke neste’în, İhdinessırâtel mustakîm. Sırâtellezine en’amte aleyhim ğayrilmağdûbi aleyhim ve leddâllîn.",
+        "anlam": "Rahmân ve rahîm olan Allah’ın adıyla. Hamd (övme ve övülme), âlemlerin Rabbi Allah’a mahsustur. O, rahmândır ve rahîmdir. Ceza gününün mâlikidir. (Rabbimiz!) Ancak sana kulluk ederiz ve yalnız senden medet umarız. Bize doğru yolu göster. Kendilerine lütuf ve ikramda bulunduğun kimselerin yolunu; gazaba uğramışların ve sapmışların yolunu değil! Âmin."
+    },
+    "Fil Suresi": {
+        "okunus": "Bismillâhirrahmanirrahim. Elem tera keyfe fe’ale rabbuke biashâbilfîl. Elem yec’al keydehum fî tadlîl. Ve ersele aleyhim tayran ebâbîl. Termîhim bihicâratin min siccîl. Fece’alehum ke’asfin me’kûl.",
+        "anlam": "Rahmân ve rahîm olan Allah’ın adıyla. Rabbin fil sahiplerine neler etti, görmedin mi? Onların kötü planlarını boşa çıkarmadı mı? Onların üstüne sürü sürü kuşlar gönderdi. O kuşlar, onların üzerlerine pişkin tuğladan yapılmış taşlar atıyordu. Böylece Allah onları yenilip çiğnenmiş ekine çevirdi."
+    },
+    "Kureyş Suresi": {
+        "okunus": "Bismillâhirrahmanirrahim. Li’î lâfi Kurayş’in. Îlâfihim rihleteşşitâi vessayf. Felya’budû rabbe hâzelbeyt. Ellezî et’amehum min cû’in ve âmenehum min havf.",
+        "anlam": "Rahmân ve rahîm olan Allah’ın adıyla. Kureyş’e kolaylaştırıldığı, evet, kış ve yaz seyahatleri onlara kolaylaştırıldığı için onlar, kendilerini açlıktan doyuran ve her çeşit korkudan emin kılan şu evin Rabbine kulluk etsinler."
+    },
+    "Maun Suresi": {
+        "okunus": "Bismillâhirrahmanirrahim. Era’eytellezî yukezzibu biddîn. Fezâlikellezî, yedu’ulyetîm. Ve lâ yehuddu alâ ta’âmilmiskîn. Feveylun lilmusallîn. Ellezîne hum an salâtihim sâhûn. Ellezîne hum yurâûne. Ve yemne’ûnelmâ’ûn.",
+        "anlam": "Rahmân ve rahîm olan Allah’ın adıyla. Dini yalanlayanı gördün mü? İşte o, yetimi itip kakar; Yoksulu doyurmaya teşvik etmez; Yazıklar olsun o namaz kılanlara ki, onlar namazlarını ciddiye almazlar. Onlar gösteriş yapanlardır; hayra da mâni olurlar."
+    },
+    "Kevser Suresi": {
+        "okunus": "Bismillâhirrahmanirrahim. İnnâ a’taynâkelkevser. Fesalli lirabbike venhar. İnne şânieke huvel’ebter.",
+        "anlam": "Rahmân ve rahîm olan Allah’ın adıyla. (Resûlüm!) Kuşkusuz biz sana Kevser’i verdik. Şimdi sen Rabbine kulluk et ve kurban kes. Asıl sonu kesik olan, şüphesiz sana hınç besleyendir."
+    },
+    "Kafirun Suresi": {
+        "okunus": "Bismillâhirrahmanirrahim. Kul yâ eyyuhel kâfirûn. Lâ a’budu mâ ta’budûn. Ve lâ entum âbidûne mâ a’bud. Ve lâ ene âbidun mâ abedtum. Ve lâ entum âbidûne mâ a’bud. Lekum dînukum veliye dîn.",
+        "anlam": "Rahmân ve rahîm olan Allah’ın adıyla. (Resûlüm!) De ki: Ey kâfirler! Ben sizin tapmakta olduklarınıza tapmam. Siz de benim taptığıma tapmıyorsunuz. Ben de sizin taptıklarınıza asla tapacak değilim. Evet, siz de benim taptığıma tapıyor değilsiniz. Sizin dininiz size, benim dinim de banadır."
+    },
+    "Nasr Suresi": {
+        "okunus": "Bismillâhirrahmanirrahim. İzâ câe nasrullahi velfeth. Ve raeytennâse yedhulûne fî dinillâhi efvâcâ. Fesebbih bihamdi rabbike vestağfirh. İnnehû kâne tevvâbâ.",
+        "anlam": "Rahmân ve rahîm olan Allah’ın adıyla. Allah’ın yardımı ve zaferi gelip de insanların bölük bölük Allah’ın dinine girmekte olduklarını gördüğün vakit Rabbine hamdederek O’nu tesbih et ve O’ndan mağfiret dile. Çünkü O, tevbeleri çok kabul edendir."
+    },
+    "Tebbet Suresi": {
+        "okunus": "Bismillâhirrahmanirrahim. Tebbet yedâ ebî lehebin ve tebb. Mâ eğnâ anhu mâluhû ve mâ keseb. Seyeslâ nâren zâte leheb. Vemraetuhû hammâletelhatab. Fî cî dihâ hablun min mesed.",
+        "anlam": "Rahmân ve rahîm olan Allah’ın adıyla. Ebu Leheb’in iki eli kurusun! Kurudu da. Malı ve kazandıkları ona fayda vermedi. O, alevli bir ateşte yanacak. Odun taşıyıcı olarak ve boynunda hurma lifinden bükülmüş bir ip olduğu halde karısı da (ateşe girecek)."
+    },
+    "İhlas Suresi": {
+        "okunus": "Bismillâhirrahmanirrahim. Kul hüvellâhü ehad. Allâhussamed. Lem yelid ve lem yûled. Ve lem yekün lehû küfüven ehad.",
+        "anlam": "Rahmân ve rahîm olan Allah’ın adıyla. De ki: O, Allah birdir. Allah sameddir. O, doğurmamış ve doğmamıştır. Onun hiçbir dengi yoktur."
+    },
+    "Felak Suresi": {
+        "okunus": "Bismillâhirrahmanirrahim. Kul e’ûzu birabbilfelak. Min şerri mâ halak. Ve min şerri ğasikın izâ vekab. Ve min şerrinneffâsâti fil’ukad. Ve min şerri hâsidin izâ hased.",
+        "anlam": "Rahmân ve rahîm olan Allah’ın adıyla. De ki: Yarattığı şeylerin şerrinden, karanlığı çöktüğü zaman gecenin şerrinden, düğümlere üfürüp büyü yapan üfürükçülerin şerrinden ve kıskandığı vakit kıskanç kişinin şerrinden sabahın Rabbine sığınırım!"
+    },
+    "Nas Suresi": {
+        "okunus": "Bismillâhirrahmanirrahim. Kul e’ûzu birabbinnâsi. Melikinnâsi. İlâhinnâs. Min şerrilvesvâsilhannâs. Ellezî yuvesvisu fî sudûrinnâsi. Minelcinneti vennâs.",
+        "anlam": "Rahmân ve rahîm olan Allah’ın adıyla. De ki: İnsanların kalplerine vesvese sokan, (insan Allah’ı andığında) pusuya çekilen cin ve insan şeytanının şerrinden insanların Rabbine, insanların Melikine (mutlak sahip ve hakimine) insanların İlâhına sığınırım!"
+    }
+}
+
+ascii_art = """
+#*+=--:. -=*#                                                                      
+                 *+:     .=.:*                                                                      
+                 *:.+*+=-:...+#               @@@@@@                                                
+                 *-.        .+#           @@@@%%##%%@@@@                                            
+                 *=.        .*#         @@%#**********#%@@@                                         
+                 #+.      .:=%@@      @@@#**************#%@@                                        
+                   *##%#******#@@@   @@%**#-.-*****#- .+#*%@@                                       
+                      @@********#@@@@@%**+.   .**#*.    .**#@@                                      
+                      @@%*********%@@%***.     .#*:      -**#@@                                     
+                       @@%#********##**#= =%..:.=+..*%-...+**%@@                                    
+                         @@#***********#--%@@@@:+*.=@@@@#.+**#@@                                    
+                          @@@#**********+.+@@@*.++:.*@@%-.+***#@@                                   
+                            @@@%#******#%-  .  -:.-.    .=#***#@@@                                  
+                               @@@%***#@@#-. .+#####.  .*%@#***##%@@@@                              
+                                 @@#*#%@*.  . =+::-%.:...-#@#*******%@@@                            
+                                 @@#*%@*.     .=*=*:      :#%********#%@@                           
+                                 @%*#%*.        .:..       :##*********#@@@                         
+                                 @%*##:                     :#***********%@@                        
+                                 @%*%=.                     .+*#*********#@@                        
+                                 @@#*:                      .-*@@%#*****++#%#                       
+                                 @@#+                        :#@@@@@@%=+  ...:-*#                   
+                                  @@=                       :*%@    #+.=:.  .:=.=*                  
+                                  @@#:                      .*@@    *- .-====: .+#                  
+                      #*****+***   @@+:.                    =@@     *.         =*                   
+                    *+:...    -=*   @@+.                   -%@     #+         .*                    
+                    *---..   :-.*    @%+.                .=%@       #**+:.....+#                    
+                    #=. :==-=-. =*    @@#:.             .+%@             #*****                     
+                    #=.         -*      @%+:          .=%@@                  *##**+==--+*           
+                     +:         -*   @@@@@@@#=:.   :=#@@                     **:      -==#          
+                     *-...::-=+*#%%+-:::::#@  @@@@@@@@%#+++++*#%@@           +:.+-..:--.:*          
+                       #*#*   @@*-::::::::=%        %=:::::::::::-+%@        +:  .::..  .*          
+                             @*:::::::::::*@        %-::::::::=%%*-:-*@      *-         .+#         
+                            %=:=@=:::::::=@         @%-:::::::::::=*@%%@     #=.       ..+#         
+                          @%:=#+::=%*:::+@            @%=::::##*=-:::::#@    #*=--==+***#           
+                          %=##:::#*-::+%                @%#+::::-+*#%#*@                            
+                          @#:::*%-::+@@                     @%%%%%%%%@@                             
+                          @*-+%=:-#@                                                                
+                             @@@@                                                                   
+"""
+
+print(ascii_art)
+print("Penguen ile Dinimi Öğreniyorum Uygulamasına Hoş Geldiniz!")
+
+# Programın sürekli çalışması için sonsuz döngü başlatıyoruz
+while True:
+    print("\n" + "="*40)
+    print("             ANA MENÜ")
+    print("="*40)
+    print("1 - Namazları öğrenmek")
+    print("2 - Sureleri öğrenmek")
+    print("3 - Duaları öğrenmek (Henüz eklenmedi)")
+    print("4 - Esmaül Hüsna'ları öğrenmek (Henüz eklenmedi)")
+    print("0 - Programdan Çıkış")
+    print("="*40)
+    
+    secim = input("Lütfen yapmak istediğiniz işlemin numarasını giriniz: ")
+    
+    if secim == "1":
+        print("\n--- NAMAZ REKATLARI ---")
+        print("Sabah Namazı toplam 4 rekatdır. 2 Rekat Sünnet 2 Rekat Farz'dır.")
+        print("Öğle Namazı toplam 10 rekatdır. 4 Rekat Sünnet 4 Farz ve 2 Rekat Son Sünnet'dir")
+        print("İkindi Namazı toplam 8 rekatdır. 4 Rekat Sünnet ve 4 Rekat Farz'dır.")
+        print("Akşam Namazı toplam 5 rekatdır. 3 Rekat Farz ve 2 Rekat Sünnetdir.")
+        print("Yatsı Namazı toplam 14 rekatdır. 4 Rekat Sünnet, 4 Rekat Farz, 2 Rekat Son Sünnet ve 3 Rekat Vitir Vacip'tir.")
+        print("\nEk bilgi: Tüm namazlarda ilk rekatta kıyamda Sübhaneke Duası okunur; oturuşlarda Tahiyyat, Allahümme Salli-Barik ve Rabbena Duaları okunur.")
+        
+        input("\nAna menüye dönmek için ENTER tuşuna basın...")
+        continue # Döngünün başına (Ana Menüye) geri döndürür
+        
+    elif secim == "2":
+        # Sure seçimi için iç döngü başlatıyoruz, böylece kullanıcı üst üste sure bakabilir
+        while True:
+            print("\n--- SURE LİSTESİ ---")
+            sure_isimleri = list(sureler.keys())
+            for sira, isim in enumerate(sure_isimleri, 1):
+                print(f"{sira} - {isim}")
+            print("0 - Ana Menüye Dön")
+            
+            try:
+                sure_secim = int(input("\nİncelemek istediğiniz surenin numarasını giriniz: "))
+                
+                if sure_secim == 0:
+                    break # İç döngüden çıkar, ana menüye döner
+                    
+                if 1 <= sure_secim <= len(sure_isimleri):
+                    secilen_sure_adi = sure_isimleri[sure_secim - 1]
+                    
+                    print(f"\n=================== {secilen_sure_adi.upper()} ===================")
+                    print(f"\n[OKUNUŞU]:\n{sureler[secilen_sure_adi]['okunus']}\n")
+                    print(f"[ANLAMI]:\n{sureler[secilen_sure_adi]['anlam']}")
+                    print("==================================================")
+                    
+                    input("\nDiğer sureleri görmek için ENTER tuşuna basın...")
+                else:
+                    print("\n[!] Geçersiz bir numara girdiniz. Lütfen listedeki sayılardan seçin.")
+            except ValueError:
+                print("\n[!] Lütfen sadece sayı giriniz.")
+                
+    elif secim == "3":
+        print("\nDualar bölümü yakında eklenecektir!")
+        input("\nAna menüye dönmek için ENTER tuşuna basın...")
+        
+    elif secim == "4":
+        print("\nEsmaül Hüsna bölümü yakında eklenecektir!")
+        input("\nAna menüye dönmek için ENTER tuşuna basın...")
+        
+    elif secim == "0":
+        print("\nPenguen sizinle öğrenmekten çok mutlu oldu! Kendinize iyi bakın, Allah'a emanet olun. Elveda! 🐧")
+        break # Ana döngüyü kırar ve programı tamamen sonlandırır
+        
+    else:
+        print("\n[!] Geçersiz bir seçim yaptınız. Lütfen ana menüdeki numaralardan birini giriniz.")
